5351d4dc07962508f31abed1b0a7ce28bbb64703
LuisRCondoriSusano <luis.condori.s@ucb.edu.bo>
2024-08-14 22:19:58 -0400

    v3

--- codigo/calculadora.py
+++ codigo/calculadora.py
@@ -5,21 +5,21 @@ def suma(n1,n2):
     sum=n1+n2
     return sum
 def resta(n1,n2):
-    res=n1+n2
+    res=n1-n2
     return res
 def multiplicacion(n1,n2):
-    mul=n1+n2
+    mul=n1*n2
     return mul
 def division(n1,n2):
-    div=n1+n2
+    div=n1/n2
     return div
 if opcion == 1:
-    print("resultado: ", sum)
+    print("resultado: ", suma(n1,n2))
 elif opcion == 2:
-    print("resultado: ", res)
+    print("resultado: ", resta(n1,n2))
 elif opcion == 3:
-    print("resultado: ", mul)
+    print("resultado: ", multiplicacion(n1,n2))
 elif opcion == 4:
-    print("resultado:", div)  
+    print("resultado:", division(n1,n2))  
 else:
     print("opcion invalida")         
