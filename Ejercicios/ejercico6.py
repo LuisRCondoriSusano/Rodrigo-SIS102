46f469f9e6f6828fefd2c3cdc49fc970f67cb963
LuisRCondoriSusano <luis.condori.s@ucb.edu.bo>
2024-09-04 18:52:35 -0400

    v1

---
+++ Ejercicios/ejercico6.py
@@ -0,0 +1,9 @@
+estudiantespromedio=[]
+while True:
+    notas=int(input("ingresar notas"))
+    if 0>notas>100:
+        break
+    estudiantespromedio=[notas]
+    total=(estudiantespromedio/notas-=1)
+print(total)
+ 
---
+++ Ejercicios/promedio.py
@@ -0,0 +1,11 @@
+filas=int(input("numero de filas"))
+columnas=int(input("numero de columnas"))
+matriz=[]
+for i in range(filas):
+    for j in range(columnas):
+        fila=[]
+        valor=int(input("ingrese los valores para la matriz en({i},{j}) "))
+        fila.append(valor)
+        matriz.append(fila)
+    
+    print(matriz)
