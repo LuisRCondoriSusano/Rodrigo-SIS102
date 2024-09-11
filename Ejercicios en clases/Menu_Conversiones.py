b80c5cbfd0eb50cec58d139a33235c1323efec88
LuisRCondoriSusano <luis.condori.s@ucb.edu.bo>
2024-09-11 19:14:23 -0400

    v1

---
+++ Ejercicios en clases/Menu_Conversiones.py
@@ -0,0 +1,30 @@
+import funcion_conversiones
+def menu():
+    print("-------------Seleccione que conversion desea hacer-------------")
+    print("1: metros a kilometros")
+    print("2: kilometros a metros")
+    print("3: gramos a kilogramos")
+    print("4: kilogramos a gramos")
+    print("5: celcius a fahrenheit")
+    print("6: fahrenheit a celcius")
+    print("7: Salir")
+menu()
+while True:
+    
+    num1=float(input("Deme un numero: ")) 
+    opcion=int(input("Seleciones que opcion desea hacer: "))
+    if opcion == 7:
+        print("Saliendo")
+        break
+    if opcion == 1:
+        print(f"Resultados: {funcion_conversiones.metrosAkilometros(num1):.2f}")
+    elif opcion == 2:
+        print(f"Resultado: {funcion_conversiones.kilometrosAmetros(num1):.2f}")
+    elif opcion == 3:
+        print(f"Resultado: {funcion_conversiones.gramosAKilogramos(num1):.2f}")
+    elif opcion == 4:
+        print(f"Resultado: {funcion_conversiones.kilogramosAgramos(num1):.2f}")
+    elif opcion == 5:
+        print(f"Resultado: {funcion_conversiones.celsiusAfah(num1):.2f}")    
+    elif opcion == 6:
+        print(f"Resultado: {funcion_conversiones.fahAcelcuis(num1):.2f}")
---
+++ Ejercicios en clases/funcion_conversiones.py
@@ -0,0 +1,13 @@
+def metrosAkilometros(num1):
+    return num1/1000
+def kilometrosAmetros(num1):
+    return num1*1000
+def gramosAKilogramos(num1):
+    return num1/1000
+def kilogramosAgramos(num1):
+    return num1*1000
+def celsiusAfah(num1):
+    return (num1*1.8) + 32
+def fahAcelcuis(num1):
+    return (num1-32)/1.8
+
